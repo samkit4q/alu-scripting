61046414bbe76dfb05ea3597d5770e58ccf03f34
samkit4q <a.aderoba@alustudent.com>
2024-03-03 05:14:27 -0800

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API and
- returns the number of subscribers (all subscribers)"""
+"""Queries the Reddit API and
+returns the number of subscribers
+(not active users, total subscribers)
+for a given subreddit.
+If an invalid subreddit is given,
+the function should return 0.
+"""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    queries the Reddit API
-    returns the number of subscribers for a given reddit
-    """
-    import json
-    import requests
-    subreddit_URL = 'https://www.reddit.com/r/{}/about/.json'.format(
-        subreddit)
-    subreddit_info = requests.get(subreddit_URL,
-                                  headers={"user-agent": "user"},
-                                  allow_redirects=False).json()
-    if "data" not in subreddit_info:
+    base_url = 'https://www.reddit.com'
+    api_uri = '{base}/r/{subreddit}/about.json'.format(base=base_url,
+                                                       subreddit=subreddit)
+    user_agent = {'User-Agent': 'Python/requests'}
+    res = requests.get(api_uri, headers=user_agent, allow_redirects=False)
+    if res.status_code in [302, 404]:
         return 0
-    subscribers = subreddit_info.get("data").get("subscribers")
-    return subscribers
+    print("OK")
+    return res.json().get('data').get('subscribers')
